f2153c1df5fdc7d57bb1ba04bef46983f6ef0af2
yarik <lepenkiny@gmail.com>
2017-05-31 17:57:34 +0300

    use file on upload

--- src/main/kotlin/recognition/MsCognitiveApi.kt
+++ src/main/kotlin/recognition/MsCognitiveApi.kt
@@ -3,9 +3,11 @@ package recognition
 import com.google.gson.Gson
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.utils.URIBuilder
+import org.apache.http.entity.FileEntity
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.util.EntityUtils
+import java.io.File
 
 val gson = Gson()
 
@@ -30,11 +32,13 @@ object CognitiveApi {
             val uri = uriBuilder.build()
             val request = HttpPost(uri)
 
-            request.setHeader("Content-Type", "application/json")
+            request.setHeader("Content-Type", "application/octet-stream")
             request.setHeader("Ocp-Apim-Subscription-Key", info.key1)
 
-            val url = "https://photos-5.dropbox.com/t/2/AADoP_f1cZuDTvkKKsw5QH1ttmdH7DL7BcQPu3w3mhgItQ/12/62224416/png/32x32/3/1496250000/0/2/img-2017-05-31-00-41-55.png/EJW5mjAYhTsgAigC/Z3kFDJfuNu9MvO8bloB5HKBnCpbPJqow3KvsyF4pVqc?dl=0&size=1600x1200&size_mode=3"
-            val requestEntity = StringEntity("{\"url\":\"$url\"}")
+
+            val file = File("xx.png")
+            val requestEntity = FileEntity(file)
+
             request.entity = requestEntity
 
             val response = httpClient.execute(request)
