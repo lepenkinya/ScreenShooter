e4add95b12b4d589843d0e73a43a48690b86fda8
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-05-31 18:03:32 +0300

    dnd: create file for the image

--- src/main/kotlin/shooter/service/ImageParsingService.kt
+++ src/main/kotlin/shooter/service/ImageParsingService.kt
@@ -4,14 +4,26 @@ import com.intellij.ide.scratch.ScratchFileService
 import com.intellij.ide.scratch.ScratchRootType
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.PathUtil
+import com.intellij.util.ui.UIUtil
 import java.awt.Image
+import java.awt.image.BufferedImage
+import java.io.File
+import java.util.concurrent.atomic.AtomicLong
+import javax.imageio.ImageIO
+
 
 class ImageParsingService(val project: Project) {
+
+    val logger = Logger.getInstance("#shooter.service.ImageParsingService")
+    val nextNumber = AtomicLong()
+
     companion object ServiceHolder {
         fun getService(project: Project): ImageParsingService {
             return ServiceManager.getService(project, ImageParsingService::class.java)
@@ -27,8 +39,7 @@ class ImageParsingService(val project: Project) {
 
 
     private fun processImageImpl(image: Image, fileType: FileType?) {
-        print(image.javaClass)
-
+        saveImageAsIOFile(image)
 
         val language = (fileType as? LanguageFileType)?.language
 
@@ -45,5 +56,29 @@ class ImageParsingService(val project: Project) {
                 FileEditorManager.getInstance(project).openFile(scratchFile, true)
             }
         })
+    }
+
+    private fun saveImageAsIOFile(image: Image): File? {
+        try {
+            val bufferedImage = UIUtil.createImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB)
+
+            val bImageGraphics = bufferedImage.createGraphics()
+            bImageGraphics.drawImage(image, null, null)
+            val outFile = FileUtil.createTempFile("saved_" + nextNumber.incrementAndGet(), ".png", true)
+
+            ImageIO.write(bufferedImage, "png", outFile)
+            logMessages("Created file: " + outFile.absolutePath)
+
+            return outFile
+        } catch (e: Exception) {
+            logMessages("Error while writing file " + e.localizedMessage)
+        }
+
+        return null
+    }
+
+    private fun logMessages(text: String) {
+        println(text)
+        logger.info(text)
     }
 }
