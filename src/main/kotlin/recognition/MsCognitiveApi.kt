65d221feebcec153e031434cd19e084a5ceac820
yarik <lepenkiny@gmail.com>
2017-05-31 18:21:12 +0300

    printing result

--- src/main/kotlin/recognition/MsCognitiveApi.kt
+++ src/main/kotlin/recognition/MsCognitiveApi.kt
@@ -11,6 +11,26 @@ import java.io.File
 
 val gson = Gson()
 
+class Region {
+    var boundingBox: String = ""
+    var lines: List<Line> = emptyList()
+}
+
+class Line {
+    var boundingBox: String = ""
+    var words: List<Word> = emptyList()
+}
+
+class Word {
+    var boundingBox: String = ""
+    var text: String = ""
+}
+
+class CognitiveResponse {
+    var language: String = ""
+    var regions: List<Region> = emptyList()
+}
+
 object CognitiveApi {
     class Info(val endpoint: String, val key1: String, val key2: String)
 
@@ -26,7 +46,7 @@ object CognitiveApi {
         try {
             val uriBuilder = URIBuilder("${info.endpoint}/ocr")
 
-            uriBuilder.setParameter("language", "unk")
+            uriBuilder.setParameter("language", "en")
             uriBuilder.setParameter("detectOrientation ", "true")
 
             val uri = uriBuilder.build()
@@ -36,7 +56,7 @@ object CognitiveApi {
             request.setHeader("Ocp-Apim-Subscription-Key", info.key1)
 
 
-            val file = File("xx.png")
+            val file = File("erodereference.png")
             val requestEntity = FileEntity(file)
 
             request.entity = requestEntity
@@ -45,7 +65,17 @@ object CognitiveApi {
             val entity = response.entity
 
             if (entity != null) {
-                println(EntityUtils.toString(entity))
+                val text = EntityUtils.toString(entity)
+                println(text)
+
+                val region = gson.fromJson(text, CognitiveResponse::class.java)
+                region.regions.forEach {
+                    println("REGION_START")
+                    it.lines.forEach {
+                        println(it.words.joinToString(" ", transform = { it.text }))
+                    }
+                    println("REGION_END")
+                }
             }
 
         } catch (e: Exception) {
