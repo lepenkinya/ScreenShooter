ee7b79606347a40fbc43e1048b1fbbdb150f6b4c
yarik <lepenkiny@gmail.com>
2017-05-31 15:33:29 +0300

    sample recognition

--- src/main/kotlin/recognition/ImageRecognition.kt
+++ src/main/kotlin/recognition/ImageRecognition.kt
@@ -1,11 +1,59 @@
 package recognition
 
-object ImageRecognition {
+import com.google.gson.Gson
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.utils.URIBuilder
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.util.EntityUtils
 
+val gson = Gson()
 
-    fun string() {
+object CognitiveApi {
+    class Info(val endpoint: String, val key1: String, val key2: String)
 
+    val info by lazy {
+        val text = CognitiveApi::class.java.classLoader.getResource("api.json").readText()
+        gson.fromJson<Info>(text, Info::class.java)
     }
 
 
+    fun check() {
+        val httpClient = DefaultHttpClient()
+
+        try {
+            val uriBuilder = URIBuilder("${info.endpoint}/ocr")
+
+            uriBuilder.setParameter("language", "unk")
+            uriBuilder.setParameter("detectOrientation ", "true")
+
+            val uri = uriBuilder.build()
+            val request = HttpPost(uri)
+
+            request.setHeader("Content-Type", "application/json")
+            request.setHeader("Ocp-Apim-Subscription-Key", info.key1)
+
+            val url = "https://photos-5.dropbox.com/t/2/AADoP_f1cZuDTvkKKsw5QH1ttmdH7DL7BcQPu3w3mhgItQ/12/62224416/png/32x32/3/1496250000/0/2/img-2017-05-31-00-41-55.png/EJW5mjAYhTsgAigC/Z3kFDJfuNu9MvO8bloB5HKBnCpbPJqow3KvsyF4pVqc?dl=0&size=1600x1200&size_mode=3"
+            val requestEntity = StringEntity("{\"url\":\"$url\"}")
+            request.entity = requestEntity
+
+            val response = httpClient.execute(request)
+            val entity = response.entity
+
+            if (entity != null) {
+                println(EntityUtils.toString(entity))
+            }
+
+        } catch (e: Exception) {
+            println(e.message)
+        }
+
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+    CognitiveApi.check()
+    println()
 }
--- src/main/kotlin/shooter/Action.kt
+++ src/main/kotlin/shooter/Action.kt
@@ -26,41 +26,4 @@ class ScreenShooter : AnAction() {
 
 
 object Main {
-    @JvmStatic fun main(args: Array<String>) {
-        val httpClient = DefaultHttpClient()
-
-        try {
-            // NOTE: You must use the same location in your REST call as you used to obtain your subscription keys.
-            //   For example, if you obtained your subscription keys from westus, replace "westcentralus" in the
-            //   URL below with "westus".
-            val uriBuilder = URIBuilder("https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr")
-
-            uriBuilder.setParameter("language", "unk")
-            uriBuilder.setParameter("detectOrientation ", "true")
-
-            val uri = uriBuilder.build()
-            val request = HttpPost(uri)
-
-            // Request headers.
-            request.setHeader("Content-Type", "application/json")
-
-            // NOTE: Replace the "Ocp-Apim-Subscription-Key" value with a valid subscription key.
-            request.setHeader("Ocp-Apim-Subscription-Key", "13hc77781f7e4b19b5fcdd72a8df7156")
-
-            // Request body. Replace the example URL with the URL of a JPEG image containing text.
-            val requestEntity = StringEntity("{\"url\":\"http://example.com/images/test.jpg\"}")
-            request.entity = requestEntity
-
-            val response = httpClient.execute(request)
-            val entity = response.entity
-
-            if (entity != null) {
-                println(EntityUtils.toString(entity))
-            }
-
-        } catch (e: Exception) {
-            println(e.message)
-        }
-
-    }
 }
