d0f7ee3dae98a8cbf8d4d0753923b855203a973a
yarik <lepenkiny@gmail.com>
2017-06-02 00:04:20 +0300

    added keywords

--- src/main/kotlin/shooter/Action.kt
+++ src/main/kotlin/shooter/Action.kt
@@ -2,6 +2,11 @@ package shooter
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiClass
+import com.intellij.psi.impl.java.stubs.index.JavaShortClassNameIndex
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.stubs.StubIndex
 import org.apache.http.util.EntityUtils
 import org.apache.http.HttpEntity
 import org.apache.http.entity.StringEntity
@@ -9,6 +14,7 @@ import org.apache.http.client.methods.HttpPost
 import org.apache.log4j.xml.DOMConfigurator.setParameter
 import org.apache.http.client.utils.URIBuilder
 import org.apache.http.impl.client.DefaultHttpClient
+import java.io.File
 
 
 class ScreenShooter : AnAction() {
@@ -20,10 +26,39 @@ class ScreenShooter : AnAction() {
 
 
     override fun actionPerformed(e: AnActionEvent) {
-        println("HEYYO")
+        val index = JavaShortClassNameIndex.getInstance()
+        val project = e.project!!
+        val keys = index.getAllKeys(project)
+
+        val set = mutableSetOf<String>()
+
+        val allScope = GlobalSearchScope.allScope(project)
+        keys.forEach { className ->
+            println("Classname: $className")
+            set.add(className)
+            val classes: Collection<PsiClass> = index.get(className, project, allScope)
+            classes.forEach {
+                it.methods.map {
+                    println("Method: ${it.name}")
+
+                    set.add(it.name)
+                    it.parameterList.parameters.mapNotNull { it.name }.forEach {
+                        set.add(it)
+                    }
+                }
+            }
+        }
+
+        val file = File("/Users/yarik/IdeaProjects/ScreenShooter/symbols/jdk")
+        if (file.exists()) {
+            file.delete()
+        }
+        file.createNewFile()
+
+        set.forEach {
+            file.appendText("$it\n")
+        }
     }
-}
 
 
-object Main {
 }
