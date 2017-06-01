f8268cb830ac391144dc79fe02b154b8cf2e0900
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-06-01 13:01:22 +0300

    simple test

--- src/test/kotlin/shooter/service/ServiceIntegrationTest.kt
+++ src/test/kotlin/shooter/service/ServiceIntegrationTest.kt
@@ -3,13 +3,17 @@ package shooter.service
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.PlainTextFileType
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+import com.intellij.util.io.readText
 import junit.framework.TestCase
 import java.io.IOException
 import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.io.File
+import java.nio.file.Paths
 
 
 class ServiceIntegrationTest : LightPlatformCodeInsightFixtureTestCase() {
@@ -52,7 +56,9 @@ class ServiceIntegrationTest : LightPlatformCodeInsightFixtureTestCase() {
 
 
         val filePath = testDataPath + getTestName(true) + ".txt"
-        val resultText = FileUtil.loadLines(filePath).joinToString(separator = "\n")
+
+
+        val resultText = StringUtil.convertLineSeparators(Paths.get(filePath).readText())
         myFixture.checkResult(resultText)
     }
 
@@ -64,6 +70,10 @@ class ServiceIntegrationTest : LightPlatformCodeInsightFixtureTestCase() {
 
     fun testUnused() {
         doTestForFile(JavaFileType.INSTANCE)
+    }
+
+    fun testCreateMethodTS() {
+        doTestForFile(PlainTextFileType.INSTANCE)
     }
 
 }
